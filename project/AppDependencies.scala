04ee97fd8889deba2535da1c2c8e1184652ecc36
Rashad Mughal <rashadmughal@users.noreply.github.com>
2020-03-20 14:27:23 +0000

    CDD-2724: Cleanup build.sbt and update dependencies (#30)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ object AppDependencies {
 
   private val hmrcTestVersion = "3.9.0-play-26"
   private val scalatestplusVersion = "3.1.3"
-  private val mockitoVersion = "3.2.4"
-  private val wireMockVersion = "2.26.0"
+  private val mockitoVersion = "3.3.1"
+  private val wireMockVersion = "2.26.3"
   private val customsApiCommonVersion = "1.46.0"
   private val simpleReactiveMongoVersion = "7.23.0-play-26"
   private val reactiveMongoTestVersion = "4.15.0-play-26"
